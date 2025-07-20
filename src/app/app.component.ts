cc1ede0ba1b340e4394b8af9417de68f3eee1570
Ravi Kumar <ravi.kumar1@assaabloy.com>
2025-07-20 16:04:32 +0530

    Fresh start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, NavbarComponent],
-  templateUrl:'./app.component.html',
-  styleUrl: './app.component.css'
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'Homes';
-  isLoggedIn = true;
-}
+export class AppComponent {}
--- src/app/utils/api-constants.ts
+++
@@ -1,5 +0,0 @@
-export const API_URL = {
-    BASE_URL: 'http://localhost:8081/',
-    GUEST: '/api/guests',
-    RESERVATION : '/api/reservation',
-  };
