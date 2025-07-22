048c6c85d1735e2ac9c580acecee4ed3fd75cab0
Ravi Kumar <ravikumartholiya@gmail.com>
2025-07-22 09:10:44 +0530

    adding user components

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
+//These are the components that will be loaded when application starts
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
+//To load the header component in starting
+// bootstrapApplication(HeaderComponent)
